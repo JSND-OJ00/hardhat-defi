4e83b2b7a88a8aa9587d5789bf2dc701eed24bcf
JSND-OJ00 <jsnd.oj@gmail.com>
2022-07-24 21:53:26 +0900

    wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
+import "solidity-coverage";
+import "hardhat-deploy";
+import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
